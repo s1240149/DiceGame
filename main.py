0ac536b5d2a6b4aadc4a14f2ab74767b9a054445
s1240149 <s1240149@std6dc42.u-aizu.ac.jp>
2018-07-08 17:04:00 +0900

    Initial revision

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+random.seed()
+
+print("Rolling the dice...")
+a=random.randint(1,6)
+b=random.randint(1,6)
+
+print("Die 1: "+str(a))
+print("Die 2: "+str(b))
+print("Total value: "+str(a+b))
